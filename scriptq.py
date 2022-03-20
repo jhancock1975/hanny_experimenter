a6153beadfc652697968045e6da151cf94e28be8
John Hancock <jhacock1975@gmail.com>
2022-03-20 11:04:36 -0400

    sync from delli

---
+++ scriptq.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import subprocess
+import logging
+from our_util import get_logger
+
+if __name__ == '__main__':
+    logger = get_logger()
+    # output of subprocess is a CompletedProcess object
+    cp = subprocess.run('squeue -u jhancoc4 -o "%N" -h"')
+    logger.debug(f'cp.stdout = {cp.stdout}')
+    
