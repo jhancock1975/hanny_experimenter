dfaae77b3edab5bc5e96711f9161f6ff9730ea02
John Hancock <jhacock1975@gmail.com>
2022-03-20 11:30:41 -0400

    sync from delli

--- scriptq.py
+++ scriptq.py
@@ -6,6 +6,10 @@ from our_util import get_logger
 if __name__ == '__main__':
     logger = get_logger()
     # output of subprocess is a CompletedProcess object
-    cp = subprocess.run(['squeue','-h', '--me', '-o', '%N'], capture_output=True)
-    logger.debug(f'cp.stdout = {cp.stdout.decode("utf-8")}')
+    # which has a stdout member when capture_output equals True
+    # stdout is a bytes array, we convert to a string, then split on newline to
+    # get a list of nodes currently in use
+    node_list = subprocess.run(['squeue','-h', '--me', '-o', '%N'],
+                               capture_output=True).decode('utf-8').split('\n')
+    logger.debug(f'node_list = {node_list}')
     
