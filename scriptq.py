9efe6ac97454abb14ac34901fbbd1d2963e4b590
John Hancock <jhacock1975@gmail.com>
2022-03-20 11:10:50 -0400

    sync from delli

--- scriptq.py
+++ scriptq.py
@@ -6,6 +6,6 @@ from our_util import get_logger
 if __name__ == '__main__':
     logger = get_logger()
     # output of subprocess is a CompletedProcess object
-    cp = subprocess.run(['squeue -u jhancoc4 -o "%N" -h"'])
+    cp = subprocess.run(['squeue', '-u jhancoc4 -o "%N" -h"'])
     logger.debug(f'cp.stdout = {cp.stdout}')
     
